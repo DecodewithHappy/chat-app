7e3bf76a6dd1eb7f00e5f932005f08018f764782
Happy <poddarsayan22@gmail.com>
2022-03-27 10:06:28 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+const config = {
+    
+        apiKey: "AIzaSyCB7C5gt0mB62chP4Hk_9R2TJ_HHoKgXC4",
+        authDomain: "chat-web-app-fe28e.firebaseapp.com",
+        projectId: "chat-web-app-fe28e",
+        storageBucket: "chat-web-app-fe28e.appspot.com",
+        messagingSenderId: "554331703565",
+        appId: "1:554331703565:web:dae08eba38af5d5b4f4844"
+      
+};
+
+const app = firebase.initializeApp(config);
