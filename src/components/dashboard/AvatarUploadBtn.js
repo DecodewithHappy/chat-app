c7500219376ad86cd4ccf332ece5492c5738fe37
Happy <poddarsayan22@gmail.com>
2022-03-30 20:07:47 +0530

    update avatar in room last message if it is changed

--- src/components/dashboard/AvatarUploadBtn.js
+++ src/components/dashboard/AvatarUploadBtn.js
@@ -5,6 +5,7 @@ import { useModalState } from '../../misc/custom-hooks';
 import { database, storage } from '../../misc/firebase';
 import { useProfile } from '../../context/profile.context';
 import ProfileAvatar from '../ProfileAvatar';
+import { getUserUpdates } from '../../misc/helpers';
 
 const fileInputTypes = '.png, .jpeg, .jpg, .jfif';
 
@@ -82,14 +83,14 @@ function AvatarUploadBtn() {
 
       const downloadUrl = await uploadAvatarResult.ref.getDownloadURL();
 
+      const updates = await getUserUpdates(
+        profile.uid,
+        'avatar',
+        downloadUrl,
+        database
+      );
 
-      const userAvatarRef = database
-        .ref(`/profiles/${profile.uid}`)
-        .child('avatar');
-
-
-      userAvatarRef.set(downloadUrl);
-
+      await database.ref().update(updates);
 
       setIsLoading(false);
       Alert.info('Avatar has been uploaded', 4000);
