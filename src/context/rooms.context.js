0c6e1c7f1d74db6f47ec88c979fb41ae85ea60ac
Happy <poddarsayan22@gmail.com>
2022-03-30 15:36:47 +0530

    created layout for chat page

--- src/context/rooms.context.js
+++ src/context/rooms.context.js
@@ -5,24 +5,24 @@ import { transformToArrWithId } from '../misc/helpers';
 const RoomsContext = createContext();
 
 export function RoomsProvider({ children }) {
-    const [rooms, setRooms] = useState(null);
+  const [rooms, setRooms] = useState(null);
 
-    useEffect(() => {
-        const roomListRef = database.ref('rooms');
+  useEffect(() => {
+    const roomListRef = database.ref('rooms');
 
-        roomListRef.on('value', snap => {
-            const data = transformToArrWithId(snap.val());
-            setRooms(data);
-        });
+    roomListRef.on('value', snap => {
+      const data = transformToArrWithId(snap.val());
+      setRooms(data);
+    });
 
-        return () => {
-            roomListRef.off();
-        };
-    }, []);
+    return () => {
+      roomListRef.off();
+    };
+  }, []);
 
-    return (
-        <RoomsContext.Provider value={rooms}>{children}</RoomsContext.Provider>
-    );
+  return (
+    <RoomsContext.Provider value={rooms}>{children}</RoomsContext.Provider>
+  );
 }
 
 export const useRooms = () => useContext(RoomsContext);
