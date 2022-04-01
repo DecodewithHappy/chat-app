f94e6505bdacfe20b0c382bd6df60d07de744751
Happy <poddarsayan22@gmail.com>
2022-04-01 21:47:25 +0530

    added button to grant or revoke admin access

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,40 +1,57 @@
-import React from 'react';
+import React, { memo } from 'react';
 import TimeAgo from 'timeago-react';
+import { Button } from 'rsuite';
 import ProfileAvatar from '../../ProfileAvatar';
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 import PresenceDot from '../../PresenceDot';
+import { useCurrentRoom } from '../../../context/current-room.context';
+import { auth } from '../../../misc/firebase';
 
-function MessageItem({ message }) {
-    const { author, createdAt, text } = message;
+function MessageItem({ message, handleAdmin }) {
+  const { author, createdAt, text } = message;
 
-    return (
-        <li className="padded mb-1">
-            <div className="d-flex align-items-center font-bolder mb-1">
-            <PresenceDot uid={author.uid} />
-            
-                <ProfileAvatar
-                    src={author.avatar}
-                    name={author.name}
-                    className="ml-1"
-                    size="xs"
-                />
+  const isAdmin = useCurrentRoom(v => v.isAdmin);
+  const admin = useCurrentRoom(v => v.admin);
 
-                <ProfileInfoBtnModal
-                    profile={author}
-                    appearance="link"
-                    className="p-0 ml-1 text-black"
-                />
-                <TimeAgo
-                    datetime={createdAt}
-                    className="font-normal text-black-45 ml-2"
-                />
-            </div>
+  const isMsgAuthorAdmin = admin.includes(author.uid);
+  const isAuthor = auth.currentUser.uid === author.uid;
+  const canGrantAdmin = isAdmin && !isAuthor;
 
-            <div>
-                <span className="word-breal-all">{text}</span>
-            </div>
-        </li>
-    );
+  return (
+    <li className="padded mb-1">
+      <div className="d-flex align-items-center font-bolder mb-1">
+        <PresenceDot uid={author.uid} />
+
+        <ProfileAvatar
+          src={author.avatar}
+          name={author.name}
+          className="ml-1"
+          size="xs"
+        />
+
+        <ProfileInfoBtnModal
+          profile={author}
+          appearance="link"
+          className="p-0 ml-1 text-black"
+        >
+          {canGrantAdmin && 
+          <Button block onClick={() => handleAdmin(author.uid)} color="blue" >
+            {isMsgAuthorAdmin ? 'Remove admin permission' : 'Give admin in this room'}
+          </Button>
+          }
+
+        </ProfileInfoBtnModal>
+        <TimeAgo
+          datetime={createdAt}
+          className="font-normal text-black-45 ml-2"
+        />
+      </div>
+
+      <div>
+        <span className="word-breal-all">{text}</span>
+      </div>
+    </li>
+  );
 }
 
-export default MessageItem;
+export default memo(MessageItem);
--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router';
+import { Alert } from 'rsuite';
 import { database } from '../../../misc/firebase';
 import { transformToArrWithId } from '../../../misc/helpers';
 import MessageItem from './MessageItem';
@@ -28,11 +29,35 @@ function Messages() {
     };
   }, [chatId]);
 
+  const handleAdmin = useCallback(async (uid) => {
+    const adminRef = database.ref(`/rooms/${chatId}/admin`);
+
+    let alertMsg;
+
+    await adminRef.transaction(admin => {
+      
+      if (admin){
+        if(admin[uid]){
+          admin[uid] = null;
+          alertMsg = 'Admin permission removed';
+        }else{
+          admin[uid] = true;
+          alertMsg = 'Admin permission granted';
+        }
+      }
+
+      return admin;
+    })
+
+    Alert.info(alertMsg, 4000)
+
+  }, [chatId])
+
   return (
     <ul className="msg-list custom-scroll">
       {isChatEmpty && <li>No messages yet</li>}
       {canShowMessages &&
-        messages.map(msg => <MessageItem key={msg.id} message={msg} />)}
+        messages.map(msg => <MessageItem key={msg.id} message={msg} handleAdmin={handleAdmin}/>)}
     </ul>
   );
 }
